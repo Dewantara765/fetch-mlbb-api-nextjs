f067d3b5001726f827bc63d5fa07b751fa39d278
dewantarasj <dewantarasj@dewantara.168.1.8>
2025-12-31 13:56:18 +0700

    interface untuk data list hero

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,26 @@ async function fetchHeroList()  {
   return res.data.data.records;
 }
 
-export default async function Home() {
-  const heroes = await fetchHeroList();
+interface HeroListData {
+  data: {
+    hero_id: number;
+    hero: {
+      data: {
+        name: string;
+        head: string;
+      };
+    };
+  };
+}
 
+export default async function Home() {
+  const heroes : HeroListData[] = await fetchHeroList();
 
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
       <div className="flex gap-20 flex-wrap px-8 pt-8">
-        {heroes.map((hero: any) => (
+        {heroes.map((hero) => (
           <div key={hero.data.hero_id} className="mb-4 shadow-xl p-4">
             <Link href={`/hero/${hero.data.hero_id}`}>
             <Image src={hero.data.hero.data.head} alt={hero.data.hero.data.name} width={100} height={100} unoptimized/>
